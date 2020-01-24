417e9c0c65f4a024bdee3e8f769e03179b5daf05
Krzysztof Drabik <KRZYSZTOF.DRABIK@jacobs.com>
2020-01-24 11:15:23 +0100

    upd

--- KoloryWPF3/Konwertery.cs
+++ KoloryWPF3/Konwertery.cs
@@ -6,18 +6,18 @@ namespace KoloryWPF3
 {
 
 
-    public class ByteToDoubleConverter : IValueConverter    
+    public class ByteToDoubleConverter : IValueConverter
     {
-        public object Convert(object value, 
+        public object Convert(object value,
                                 Type targetType,
-                                object parameter, 
+                                object parameter,
                                 System.Globalization.CultureInfo culture)
         {
             return (double)(byte)value;
         }
-        public object ConvertBack(object value, 
-                                    Type targetType, 
-                                    object parameter, 
+        public object ConvertBack(object value,
+                                    Type targetType,
+                                    object parameter,
                                     System.Globalization.CultureInfo culture)
         {
             return (byte)(double)value;
@@ -35,7 +35,49 @@ namespace KoloryWPF3
         public object ConvertBack(object value, Type targetType, object parameter,
         System.Globalization.CultureInfo culture)
         {
-        throw new NotImplementedException();
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class SkładoweRGBToSolidColorBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+        {
+            byte r = (byte)values[0];
+            byte g = (byte)values[1];
+            byte b = (byte)values[2];
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            SolidColorBrush pędzel = value as SolidColorBrush;
+            if (pędzel != null)
+            {
+                Color kolor = pędzel.Color;
+                return new object[3] { kolor.R, kolor.G, kolor.B };
+            }
+            else //???
+            {
+                return null;
+            }
+        }
+    }
+
+    public class SkładoweRGBDoubleToSolidColorBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+      System.Globalization.CultureInfo culture)
+        {
+            byte r = (byte)(double)values[0];
+            byte g = (byte)(double)values[1];
+            byte b = (byte)(double)values[2];
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 
